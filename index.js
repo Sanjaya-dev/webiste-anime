52547440f557f0a666935b185d37e23a3dc56f05
Fernanda <sanjaya.dev20@gmail.com>
2024-01-19 00:25:52 +0700

    create navbar and main content in file index.ejs

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
-const API_URL = "https://api.jikan.moe/v4/anime/";
+app.use(express.static('public'));
+
+// const API_URL = "https://api.jikan.moe/v4/anime/";
+
+app.get("/",async (req,res) => {
+    // const randomId = Math.floor(Math.random() * 100);
+    // const respon = await axios.get(API_URL + randomId);
+    // const result = respon.data;
+
+    res.render("index.ejs");
+    
+});
 
 app.listen(port,()=>{
     console.log("server run in port: ",port);
-})
+});
