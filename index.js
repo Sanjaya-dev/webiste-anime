21243a89df19bfbc0d06a4da49b5a9c29018fc99
Fernanda <sanjaya.dev20@gmail.com>
2024-01-19 21:58:10 +0700

    import data from api in index.ejs content

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// const API_URL = "https://api.jikan.moe/v4/anime/";
+const API_URL = "https://api.jikan.moe/v4/anime/";
 
-app.get("/",async (req,res) => {
-    // const randomId = Math.floor(Math.random() * 100);
-    // const respon = await axios.get(API_URL + randomId);
-    // const result = respon.data;
+app.get("/", async (req, res) => {
+  const randomId = Math.floor(Math.random() * 100);
+  const respon = await axios.get(API_URL + randomId);
+  const result = respon.data.data;
 
-    res.render("index.ejs");
-    
+  res.render("index.ejs", {
+    content: result,
+  });
 });
 
-app.listen(port,()=>{
-    console.log("server run in port: ",port);
+app.listen(port, () => {
+  console.log("server run in port: ", port);
 });
